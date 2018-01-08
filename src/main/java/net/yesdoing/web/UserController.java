d74292603f2afc652affe971cb7367188254a8da
Jane Lee <Yesdoing13@gmail.com>
2018-01-08 17:34:55 +0900

    로그인 기능 구현완

--- src/main/java/net/yesdoing/web/UserController.java
+++ src/main/java/net/yesdoing/web/UserController.java
@@ -1,5 +1,7 @@
 package net.yesdoing.web;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,6 +20,31 @@ public class UserController {
 
 	@Autowired
 	UserRepository userRepository;
+	
+	@GetMapping("/loginForm")
+	public String loginForm() {
+		return "user/login";
+	}
+	
+	@PostMapping("/login")
+	public String login(String userId, String password, HttpSession session) {
+		User user = userRepository.findByUserId(userId);
+		if(user == null) {
+			System.out.println("Login Failed!");
+			return "redirect:/users/loginForm";
+		}
+		
+		if(!password.equals(user.getPassword())) {
+			System.out.println("Login Failed!");
+			return "redirect:/users/loginForm"; 
+		}
+		
+		System.out.println("Login success!");
+		session.setAttribute("user", user);
+		
+		return "redirect:/";
+	}
+	
 	
 	@GetMapping("/form")
 	public String createForm() {
