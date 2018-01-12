e4696af4641d1657b06a9013c16dddeea9027be1
Jane Lee <Yesdoing13@gmail.com>
2018-01-12 16:40:47 +0900

    질문 목록에 답변 수 보여주기 기능 추가

--- src/main/java/net/yesdoing/domain/Question.java
+++ src/main/java/net/yesdoing/domain/Question.java
@@ -15,6 +15,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.OrderBy;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
 public class Question {
@@ -30,6 +31,9 @@ public class Question {
 	
 	@Lob
 	private String contents;
+	
+	@JsonProperty
+	private Integer countOfAnswer = 0;
 	
 	private LocalDateTime createDate;
 	
@@ -75,6 +79,10 @@ public class Question {
 	public String getContents() {
 		return contents;
 	}
+	
+	public Integer getCountOfAnswer() {
+		return countOfAnswer;
+	}
 
 	public boolean matchUser(User sessionUser) {
 		if(sessionUser == null) {
@@ -87,6 +95,47 @@ public class Question {
 		this.title = title;
 		this.contents = contents;
 	}
+	
+	public void addAnswer() {
+		this.countOfAnswer += 1;
+	}
+	
+	public void deleteAnswer() {
+		this.countOfAnswer -= 1;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Question other = (Question) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Question [id=" + id + ", writer=" + writer + ", title=" + title + ", contents=" + contents
+				+ ", countOfAnswer=" + countOfAnswer + ", createDate=" + createDate + ", answers=" + answers + "]";
+	}
+
+
 	
 	
 	
